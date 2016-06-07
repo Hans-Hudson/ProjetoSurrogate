fb8e12477679d46817f25a205799c1e4135aad81
Hans-Hudson <hans.hudson.braga@gmail.com>
2016-06-07 17:13:29 -0300

    Versao tel

---
+++ ProjetoSurrogate/ProjetoSurrogateWEB/App_Start/WebApiConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace ProjetoSurrogateWEB.App_Start
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Libera acesso de outras aplicaçoes
+                config.EnableCors(new EnableCorsAttribute("*", "*", "*"));
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+            config.Routes.MapHttpRoute(
+                name: "WithActionApi",
+                routeTemplate: "api/{controller}/{action}/{_codigo}",
+                defaults: new { _codigo = RouteParameter.Optional }
+            );
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{_codigo}",
+                defaults: new { _codigo = RouteParameter.Optional }
+            );
+            // WebAPI when dealing with JSON & JavaScript!
+            // Setup json serialization to serialize classes to camel (std. Json format)
+            var formatter = GlobalConfiguration.Configuration.Formatters.JsonFormatter;
+            formatter.SerializerSettings.ContractResolver =
+                new Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver();
+        }
+    }
+} 
--- ProjetoSurrogate/ProjetoSurrogateWEB/Global.asax.cs
+++ ProjetoSurrogate/ProjetoSurrogateWEB/Global.asax.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using ProjetoSurrogateWEB.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
 
@@ -12,6 +14,7 @@ namespace ProjetoSurrogateWEB
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
     }
