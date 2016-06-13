9b88567270040c99133d33386b7d3e810f6cf587
Hans-Hudson <hans.hudson.braga@gmail.com>
2016-06-13 17:50:18 -0300

    Bundle, tema css e tutorial

---
+++ ProjetoSurrogate/ProjetoSurrogateWEB/App_Start/BundleConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace ProjetoSurrogateWEB.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/css/cssBundle").Include(
+                "~/Content/bootstrap.css",
+                "~/Content/bootstrap-select.min.css",
+                "~/Content/Site.css"
+            ));
+
+            bundles.Add(new ScriptBundle("~/jsBundle").Include(
+                "~/Scripts/bootstrap.min.js",
+                "~/Scripts/jquery-2.2.3.min.js",
+                "~/Scripts/modernizr-2.6.2.js"
+            ));
+
+            // TODO: mudar isso para true antes de implantar em produção
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
--- ProjetoSurrogate/ProjetoSurrogateWEB/Global.asax.cs
+++ ProjetoSurrogate/ProjetoSurrogateWEB/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace ProjetoSurrogateWEB
@@ -16,6 +17,7 @@ namespace ProjetoSurrogateWEB
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
